5fcb8846e5a230f865d7bcc2a857bd7e73fb2680
lkirwan <liam.p.kirwan@gmail.com>
2021-11-07 21:25:41 -0500

    plug into Travis and Coveralls

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cb_falcon
+[![Build Status](https://app.travis-ci.com/lkirwan/cb_falcon.svg?branch=main)](https://app.travis-ci.com/lkirwan/cb_falcon)
+[![Coverage Status](https://coveralls.io/repos/github/lkirwan/cb_falcon/badge.svg?branch=main)](https://coveralls.io/github/lkirwan/cb_falcon?branch=main)
--- src/test/java/com/example/springboot/HelloControllerIT.java
+++ src/test/java/com/example/springboot/HelloControllerIT.java
@@ -1,7 +1,6 @@
 package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,8 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIT {
 
-	@Autowired
-	private TestRestTemplate template;
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     public void getHello() throws Exception {
